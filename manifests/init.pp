f9c7c2a59009d341b7d0a24ee53b0499f66ccaba
Garrett Honeycutt <code@garretthoneycutt.com>
2015-08-05 12:30:35 -0400

    Fix line for style

--- manifests/init.pp
+++ manifests/init.pp
@@ -95,7 +95,7 @@ class nisclient(
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => template("nisclient/yp.conf.erb"),
+        content => template('nisclient/yp.conf.erb'),
         require => Package[$my_package_name],
         notify  => Exec['ypdomainname'],
       }
